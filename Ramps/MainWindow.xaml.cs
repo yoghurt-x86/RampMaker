19025c7ae88bbc286b2df09187253c0bda809f02
tobj <tobj@itu.dk>
2018-01-02 19:01:54 +0100

    Quick UI update

--- Ramps/MainWindow.xaml.cs
+++ Ramps/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -27,6 +28,14 @@ namespace Ramps
             InitializeComponent();
             _vm = new MainViewModel();
             DataContext = _vm;
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var p0 = new Point3D(Double.Parse(p0X.Text), Double.Parse(p0Y.Text), Double.Parse(p0Z.Text));
+            var p1 = new Point3D(Double.Parse(p1X.Text), Double.Parse(p1Y.Text), Double.Parse(p1Z.Text));
+            var p2 = new Point3D(Double.Parse(p2X.Text), Double.Parse(p2Y.Text), Double.Parse(p2Z.Text));
+            var p3 = new Point3D(Double.Parse(p3X.Text), Double.Parse(p3Y.Text), Double.Parse(p3Z.Text));
+            _vm.updateBezier(p0, p1, p2, p3, Int32.Parse(Segments.Text));
         }
     }
 }
--- Ramps/ViewModels/MainViewModel.cs
+++ Ramps/ViewModels/MainViewModel.cs
@@ -24,8 +24,8 @@ namespace Ramps.ViewModels
             var point2 = new Point3D(30, 0, 0);
             var point3 = new Point3D(40, 0, 20);
             var point4 = new Point3D(50, 0, 20);
-            AddCurvedBezier(point1, point2, point3, point4);
-
+            AddCurvedBezier(point1, point2, point3, point4, 10);
+            /*
             point1 = new Point3D(0, 0, 0);
             point2 = new Point3D(0, 30, 0);
             point3 = new Point3D(0, 40, 20);
@@ -79,12 +79,18 @@ namespace Ramps.ViewModels
                     Positions = new Point3DCollection { new Point3D(0, 0, 0), new Point3D(100, 100, 0), new Point3D(100, 100, 100) },
                     TriangleIndices = new Int32Collection { 0,1,2}
                 }, Material = new DiffuseMaterial { Brush = Brushes.Gray }
-            });
+            }); */
         }
 
-        private void AddCurvedBezier(Point3D point1, Point3D point2, Point3D point3, Point3D point4)
+        internal void updateBezier(Point3D p0, Point3D p1, Point3D p2, Point3D p3, int segments)
         {
-            var points = new Bezier3D(point1, point2, point3, point4).GetPoints(20).ToArray();
+            Ramps.Children.Clear();
+            AddCurvedBezier(p0, p1, p2, p3, segments);
+        }
+
+        private void AddCurvedBezier(Point3D point1, Point3D point2, Point3D point3, Point3D point4, int segments)
+        {
+            var points = new Bezier3D(point1, point2, point3, point4).GetPoints(segments).ToArray();
             var ramps = new List<SurfSegment>();
             for (int i = 1; i < points.Count(); i++)
             {
